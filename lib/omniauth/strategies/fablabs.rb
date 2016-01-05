77da9012bc0818692288627f5e1e70838b204e83
Fiore Basile <fiore.basile@gmail.com>
2016-01-05 16:54:04 +0100

    added additional fields

--- lib/omniauth/strategies/fablabs.rb
+++ lib/omniauth/strategies/fablabs.rb
@@ -11,18 +11,22 @@ module OmniAuth
         # :authorize_path => "/oauth/authorize"
       }
 
-      uid { raw_info["id"] }
+      uid { raw_info["user"]["id"] }
 
       info do
         {
-          :email => raw_info["email"]
+          :email => raw_info["user"]["email"],
+          :first_name => raw_info["user"]["first_name"],
+          :last_name => raw_info["user"]["last_name"],
+          :name => raw_info["user"]["username"],
+          :avatar => raw_info["user"]["avatar"],
           # and anything else you want to return to your API consumers
         }
       end
 	  
 	  extra do
         {
-          'raw_info' => raw_info
+          'raw_info' => raw_info["user"]
         }
       end
 
