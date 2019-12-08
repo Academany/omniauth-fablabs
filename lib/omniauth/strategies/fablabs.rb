4824f0daa8caff7f8a2fca6fdffa70bf4ad016dd
Viktor Smari <viktorsmari@gmail.com>
2019-12-08 16:52:40 +0100

    v0.0.4 Rubocop syntax fix

--- lib/omniauth/strategies/fablabs.rb
+++ lib/omniauth/strategies/fablabs.rb
@@ -6,27 +6,26 @@ module OmniAuth
       # change the class name and the :name option to match your application name
       option :name, "fablabs"
 
-      option :client_options, {
-        :site => "https://api.fablabs.io"
-        # :authorize_path => "/oauth/authorize"
-      }
+      option :client_options,
+             site: "https://api.fablabs.io"
+      # :authorize_path => "/oauth/authorize"
 
       uid { raw_info["user"]["id"] }
 
       info do
         {
-          :email => raw_info["user"]["email"],
-          :first_name => raw_info["user"]["first_name"],
-          :last_name => raw_info["user"]["last_name"],
-          :nickname => raw_info["user"]["username"],
-          :name => raw_info["user"]["username"],
-          :image => raw_info["user"]["avatar"],
-          :avatar => raw_info["user"]["avatar"]
+          email: raw_info["user"]["email"],
+          first_name: raw_info["user"]["first_name"],
+          last_name: raw_info["user"]["last_name"],
+          nickname: raw_info["user"]["username"],
+          name: raw_info["user"]["username"],
+          image: raw_info["user"]["avatar"],
+          avatar: raw_info["user"]["avatar"]
           # and anything else you want to return to your API consumers
         }
       end
-	  
-	  extra do
+
+      extra do
         {
           'raw_info' => raw_info["user"]
         }
@@ -34,9 +33,8 @@ module OmniAuth
 
       def raw_info
         @info ||= access_token.get('/0/me.json').parsed
-        @raw_info = { "user" => @info } 
+        @raw_info = { "user" => @info }
       end
-    
     end
   end
 end
